06c448d61899352d84dedccc658ee84840d89b7b
doterodesign@gmail.com <doterodesign@gmail.com>
2020-01-05 23:35:49 -0500

    change figma embed to button link to prototype

--- js/contentList.js
+++ js/contentList.js
@@ -721,8 +721,7 @@ var caseStudies = [
             name: 'View Prototype',
             heading: 'Prototype Walkthrough',
             content: 'This is an ongoing project and the screens are being updated constantly. Please disregard inconsistencies in elements since I hop on whenever I can to keep working. Not everything can be navigated, but it will give a general idea of how the app may function.',
-            proto: 'https://www.figma.com/proto/dVg3M7TD36D1EGTXUgxNSu0T/Hi-Fidelity-Prototype?node-id=1%3A1113&viewport=1572%2C1533%2C0.3458743393421173&scaling=contain',
-
+            p: 'https://www.figma.com/proto/dVg3M7TD36D1EGTXUgxNSu0T/In-progress?node-id=1%3A4886&viewport=1994%2C1387%2C0.31970152258872986&scaling=min-zoom',
         },
     },
 
